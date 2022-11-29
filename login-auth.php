b4f7e981e8a2834499016442f75dd22a2501b5df
Rober261 <robertodifu@gmail.com>
2022-11-29 03:06:18 +0000

    login-auth Completed

--- login-auth.php
+++ login-auth.php
@@ -1,45 +1,48 @@
 <?php
-	include("includes/include-connection.php");
-	if (isset($_POST['btn-login'])) {
-		$email = $_POST['email'];
-		$pass = $_POST['pass'];
-		
-		$sql ="SELECT * FROM authors WHERE email = '$email' and password= '$pass'";
-		$result = $conn->query($sql);
-		$row = $result->fetch_array();
-		if($row){
-			header('Location: ./gallery.php');
-		}else{
-			$aux = true;
-		}
+include("includes/include-connection.php");
+$aux=true;
+if (isset($_POST['btn-login'])) {
+	$email = $_POST['email'];
+	$pass = $_POST['pass'];
+
+	$sql = "SELECT * FROM authors WHERE email = '$email' and password= '$pass'";
+	$result = $conn->query($sql);
+	$row = $result->fetch_array();
+	if ($row) {
+		header('Location: ./gallery.php');
+	} else {
+		$aux = false;
 	}
-	$aux = false;
+}
+$aux = false;
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="login/form/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="login/form/images/icons/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="login/form/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="login/form/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="login/form/vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="login/form/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="login/form/vendor/select2/select2.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="login/form/css/util.css">
 	<link rel="stylesheet" type="text/css" href="login/form/css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body>
-	
+
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
@@ -47,12 +50,12 @@
 					<img src="login/form/images/img-01.png" alt="IMG">
 				</div>
 
-				<form class="login100-form validate-form" method ="post">
+				<form class="login100-form validate-form" method="post">
 					<span class="login100-form-title">
 						Login
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -60,7 +63,7 @@
 						</span>
 					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="pass" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -77,6 +80,16 @@
 							Login
 						</button>
 					</div>
+					<div>
+						<?php
+						if (isset($_POST['btn-login'])) {
+							echo "<br>";
+							if (!$aux) {
+								echo "<div class='alert alert-error'>Credenciales incorrectas</div>";
+							}
+						}
+						?>
+					</div>
 					<div class="text-center p-t-136">
 						<a class="txt2" href="createUser.php">
 							Create your Account
@@ -87,26 +100,27 @@
 			</div>
 		</div>
 	</div>
-	
-	
 
-	
-<!--===============================================================================================-->	
+
+
+
+	<!--===============================================================================================-->
 	<script src="login/form/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="login/form/vendor/bootstrap/js/popper.js"></script>
 	<script src="login/form/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="login/form/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="login/form/vendor/tilt/tilt.jquery.min.js"></script>
-	<script >
+	<script>
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="login/form/js/main.js"></script>
 
 </body>
+
 </html>
