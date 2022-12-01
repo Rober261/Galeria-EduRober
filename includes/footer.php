c37d3f13d1435cc1be289a359c094a699dcb2564
SelkaChan <eduardochineacl@gmail.com>
2022-12-01 18:59:22 +0000

    FooterStyle fixed V.Final 1.0

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,13 @@
 <link rel="stylesheet" href="../templatesStyles/styles/footer.css">
 <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon">
-<footer>
-  <p>
+<footer class="align-middle">
+  <div>
+    <p>
     <span> Eduardo Chinea Clemente</span>
-    <span>Roberto Díaz Fumero</span>
-    <span>2ºDAW</span>
-    <span>IES Las Galletas 2022/2023</span>
-  </p>
+      <span>Roberto Díaz Fumero</span>
+      <span>2ºDAW</span>
+      <span>IES Las Galletas 2022/2023</span>
+    </p>
+  </div>
 
 </footer>
